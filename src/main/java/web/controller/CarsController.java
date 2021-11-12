c3e5ab3a981e7f8bf82f7f7f53100b0ab1994163
Alexander <sasbr@mail.ru>
2021-11-12 20:27:25 +0300

    222dao1

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -2,7 +2,8 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.CarService.CarService;
@@ -17,7 +18,7 @@ public class CarsController {
 
 
     @GetMapping(value = "/cars")
-    public String printCar(@RequestParam(value = "count", defaultValue = "5", required = false) Integer count, ModelMap model) {
+    public String printCar(@RequestParam(value = "count", defaultValue = "5", required = false) Integer count, Model model) {
 
         model.addAttribute("car", carService.getAllCars(count));
 
